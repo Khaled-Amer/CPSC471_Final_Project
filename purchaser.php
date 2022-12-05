75e11d45e87613ca07dab510b25633149263965a
InfiniteTachyon <t60.david@gmail.com>
2022-12-05 03:57:36 -0700

    :)

--- purchaser.php
+++ purchaser.php
@@ -1,3 +1,51 @@
+<?php 
+session_start();
+//$FlightID = $_POST['FlightID'];
+?>
+
+<?php 
+
+function derivingAllFlights() {
+    $con=mysqli_connect("localhost","student","ensf","471");
+    $query="SELECT Flight_Number, Departure_Time FROM FLIGHT";
+
+    $result = mysqli_query($con,$query);
+
+    $derivingSuccess =  mysqli_num_rows($result);
+
+    if ($derivingSuccess > 0) {
+        while ($row = mysqli_fetch_row($result)) {
+            echo "<br>";
+            echo "<p><b>FlightID:</b> ".$row[0]." <b>DepartureTime:</b> ".$row[1]."</p>";
+        }
+    }
+
+
+    mysqli_close($con);
+}
+
+
+?>
+
 <?php
 echo "LOGIN PURCHASER SUCCESS";
+derivingAllFlights();
+if (isset( $_POST['FlightID'])) {
+    echo "Flight: ".  $_POST['FlightID'];
+}
 ?>
+
+
+<html>
+    <body>
+        <div class="flight-chooser">
+            <form class="login-form" method="POST">
+                <label for="FlightNumber" class="form-label">FlightNumber:</label>
+                <input type="text" id="FlightNumber" name="FlightNumber" placeholder="Enter the Flight Number">
+                <input type="submit" value="Submit" class="form-button">
+                <input type="reset" value="Reset" class="form-button">
+            </form>
+
+        </div>
+    </body>
+</html>
