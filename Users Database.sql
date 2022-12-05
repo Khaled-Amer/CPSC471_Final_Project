33d223e0844ec0e83ff2cd9fd5487f84b9526e4f
InfiniteTachyon <t60.david@gmail.com>
2022-12-05 02:36:06 -0700

    Updated database with test entries

--- Users Database.sql
+++ Users Database.sql
@@ -91,10 +91,9 @@ CREATE TABLE PLANE
 );
 
 CREATE TABLE FLIGHT
-	(Flight_Number	INT			NOT NULL,
+	(Flight_Number	INT			NOT NULL	AUTO_INCREMENT,
 	 Departure_Time	DATETIME		NOT NULL,
 	 Arrival_Time 	DATETIME		NOT NULL,
-	 Flight_Time		DATETIME		NOT NULL,
  Airline_Name	VARCHAR(255)		NOT NULL,
  Plane_Serial 	VARCHAR(255)		NOT NULL,
  Arrival_Runway	VARCHAR(255),
@@ -162,8 +161,39 @@ CREATE TABLE REQUIRES
 		ON DELETE CASCADE		ON UPDATE CASCADE
 );
 
+-- Inserting Fake Data
+INSERT INTO USER (Username, SSN, Password, Fname, Lname, Bdate, Sex) VALUE 
+('user123', 123456789, 'password123', 'Fanny', 'Longbottom', '2002-02-02','F'),
+('funny', 666666666, 'pass', 'Mahmood', 'Moussavi', '1900-09-09', 'M');
 
+INSERT INTO STAFF (Username, SSN, Job) VALUE
+('funny', 666666666, 'PlaceholderJob');
 
+INSERT INTO PASSENGER (P_ID, Passport_Num) VALUE
+(111111111, 'AAAAAA');
 
+INSERT INTO PURCHASER (Username, SSN, P_ID) VALUE
+('user123', 123456789, 111111111);
 
+INSERT INTO AIRLINE (Name, Country) VALUE 
+('Data Airlines', 'Canada'),
+('Korean Airlines', 'North Korea');
 
+INSERT INTO FUEL (Type, Price) VALUE
+('Jet Fuel', 1000),
+('AVGAS', 2000);
+
+INSERT INTO PLANE (Plane_Serial, Tank_Size, Passenger_Capacity, Type, Airline_Name) VALUE 
+('PS2222', 5000, 100, 'AVGAS', 'Korean Airlines'),
+('PS4444', 10000, 350, 'Jet Fuel', 'Data Airlines');
+
+INSERT INTO GATE (Gate_Number, Location, Size) VALUE 
+('B7','South B Exit','Medium'),
+('C4', 'North C Entrance', 'Large');
+
+INSERT INTO RUNWAY (Runway_Number, Runway_Length) VALUE 
+('06L', 1000),
+('22R', 1500);
+
+INSERT INTO FLIGHT (Departure_Time, Arrival_Time, Airline_Name, Plane_Serial, Arrival_Runway, Depart_Runway, Gate_Number) VALUE 
+('2022-12-10 01:00:00', '2022-12-10 01:00:00', 'Korean Airlines', 'PS4444', '06L', '06L', 'C4');
